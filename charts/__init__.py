a6aec3c87046817621a699c5805f463d84e4700d
victorAct <lin_victor@126.com>
2017-07-19 15:43:13 +0800

    use 2to3 to make the project work in python3

--- charts/__init__.py
+++ charts/__init__.py
@@ -2,12 +2,12 @@ __author__ = 'arnout'
 
 __version__ = '0.4.6'
 
-from plot import plot, plotasync, line, area, spline, pie
-from server import run_server
+from .plot import plot, plotasync, line, area, spline, pie
+from .server import run_server
 
 from IPython.core import getipython
 from IPython.core.display import display, HTML
 
-from settings import default_settings, default_options, load_options
-import data
-import jsonencoder
+from .settings import default_settings, default_options, load_options
+from . import data
+from . import jsonencoder
